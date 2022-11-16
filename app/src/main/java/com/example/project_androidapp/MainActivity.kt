a50b2ca43dba1b5cfe944a0ae2cd68d76bf22cd4
OverDlive <donghyeok7312@naver.com>
2022-11-16 21:10:34 +0900

    second commit

--- app/src/main/java/com/example/project_androidapp/MainActivity.kt
+++ app/src/main/java/com/example/project_androidapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.project_androidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,10 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        btn.setOnClickListener {
-            Toast.makeText(this, "토스트", Toast.LENGTH_SHORT).show()
-        }
-
+    }
+    fun DDuk(v : View) {
+        tv.text = "뚝"
     }
 }
